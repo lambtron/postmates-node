b8ec6fc8e138dc2597d2b8647dd0015b79353435
andy jiang <andyjiang@gmail.com>
2014-12-10 20:07:41 -0800

    fixing up test directory

--- test/index.js
+++ test/index.js
@@ -1,10 +1,9 @@
-var server = require('./server');
+
+/**
+ * Test dependencies.
+ */
+
 var Mocha = require('mocha');
 var fs = require('fs');
 var path = require('path');
 
-var mocha = new Mocha({ reporter: 'spec' });
-mocha.addFile(path.join(__dirname, 'tests.js'));
-mocha.run(function(failures){
-  process.exit(failures);
-});
